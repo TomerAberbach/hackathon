7046a3ea8836f7ae0cd814f9ab743d46b3d186ab
Tom <torth212@gmail.com>
2019-09-10 15:16:45 -0400

    fix: add all mlh features to validate_presence_of

--- src/app/models/hacker.rb
+++ src/app/models/hacker.rb
@@ -3,5 +3,7 @@ class Hacker < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates_presence_of :checked_in, :waitlisted
+  validates_presence_of :checked_in, :waitlisted, :first_name, :last_name, :level_of_study,
+                        :major, :shirt_size, :dietary_restrictions, :special_needs, :date_of_birth,
+                        :gender, :education
 end
