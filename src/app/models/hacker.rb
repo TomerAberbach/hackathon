dbb46feaa4b640f28cffe626cabfff4dd0bbd034
Tom <torth212@gmail.com>
2019-09-10 14:57:53 -0400

    feat: add hacker model

---
+++ src/app/models/hacker.rb
@@ -0,0 +1,8 @@
+class Hacker < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates_presence_of :checked_in, :waitlisted
+
+end
