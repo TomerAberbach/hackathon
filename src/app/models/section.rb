c05f5d443be5555775db69e0a4234dca1329fcc4
Tomer Aberbach <tomeraberbach@gmail.com>
2019-11-24 22:14:58 -0500

    feat/docs(section): adds section model validation and rich text association.

--- src/app/models/section.rb
+++ src/app/models/section.rb
@@ -1,2 +1,13 @@
+##
+# A class representing a section in the home page of this website.
+# Instances of this class represent rows in the +sections+ table on the database.
 class Section < ApplicationRecord
+  # Validation
+  validates_presence_of :title, :content
+
+  # Associations
+  has_rich_text :content
+
+  # Transformation
+  text_writer :title
 end
