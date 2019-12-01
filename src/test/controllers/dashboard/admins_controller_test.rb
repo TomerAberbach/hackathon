766032d59b737514c4376dcb5020fdfe4002c486
Tom Orth <torth212@gmail.com>
2019-12-01 16:06:00 -0500

    Add Admin Controller

---
+++ src/test/controllers/dashboard/admins_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Dashboard::AdminsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
--- src/test/models/admin_test.rb
+++ src/test/models/admin_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
-class UserTest < ActiveSupport::TestCase
+class AdminTest < ActiveSupport::TestCase
   # tests to make sure super_admin? returns true for the first entry
   test "super_admin? true" do
-     assert users(:one).super_admin?
+     assert admins(:one).super_admin?
   end
 
   # tests to make sure super_admin? returns false for the second entry
   test "super_admin? false" do
-     assert_not users(:two).super_admin?
+     assert_not admins(:two).super_admin?
   end
 end
