db92ce380304ff1ee7d73d75cad69a8036902cb4
Tomer Aberbach <tomeraberbach@gmail.com>
2019-12-20 19:25:05 -0500

    fix: no port for mailer in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: "#{ENV['DOMAIN']}:#{ENV['PORT']}" }
+  config.action_mailer.default_url_options = { host: ENV['DOMAIN'] }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
